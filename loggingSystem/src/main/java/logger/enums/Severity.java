30477d0556c3456260eb6dc152c4ab9f62dc7d1b
Crpedeim <89583909+Crpedeim@users.noreply.github.com>
2025-09-26 15:12:09 +0530

    Add files via upload

---
+++ loggingSystem/src/main/java/logger/enums/Severity.java
@@ -0,0 +1,24 @@
+package logger.enums;
+
+import java.io.Serializable;
+
+public enum Severity implements Serializable {
+    LOW("low"),
+    HIGH("high"),
+    WARN("warn");
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    Severity(String name){
+        this.name = name;
+    }
+
+}
